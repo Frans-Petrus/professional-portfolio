874f5bbcb482cb3fd41cbe701077a10d8666da5c
FP du Toit <franspetrusdt@gmail.com>
2026-07-21 16:19:19 +0200

    Create JavaScript foundation for academic records

---
+++ js/academics.js
@@ -0,0 +1,111 @@
+// ======================================
+// Academic Information
+// Frans Petrus (FP) du Toit
+// ======================================
+
+const academicSummary = {
+
+    degree: "Bachelor of Science in Information Technology",
+
+    university: "North-West University",
+
+    studyMode: "Distance Learning",
+
+    expectedGraduation: "December 2026",
+
+    distinctions: 18,
+
+    completedModules: 27,
+
+    matricAverage: 83.14
+
+};
+
+
+const academicYears = [
+
+    {
+
+        year: "2026",
+
+        semester: "First Semester",
+
+        modules: [
+
+            {
+                code: "CMPG313",
+                name: "Artificial Intelligence",
+                mark: 75
+            },
+
+            {
+                code: "CMPG315",
+                name: "Computer Networks",
+                mark: 68
+            },
+
+            {
+                code: "CMPG311",
+                name: "Databases",
+                mark: 75
+            },
+
+            {
+                code: "CMPG312",
+                name: "Decision Support Systems I",
+                mark: 65
+            }
+
+        ]
+
+    },
+
+    {
+
+        year: "2025",
+
+        semester: "Second Semester",
+
+        modules: [
+
+            {
+                code: "CMPG222",
+                name: "Data Analytics II",
+                mark: 70
+            },
+
+            {
+                code: "CMPG221",
+                name: "Data Structures & Algorithms",
+                mark: 60
+            },
+
+            {
+                code: "MTHS225",
+                name: "Discrete Mathematics",
+                mark: 86
+            },
+
+            {
+                code: "BMAN223",
+                name: "Problem Solving for Managers",
+                mark: 81
+            },
+
+            {
+                code: "CMPG223",
+                name: "System Analysis & Design II",
+                mark: 68
+            },
+
+            {
+                code: "WVNS221",
+                name: "Understanding the Natural World",
+                mark: 83
+            }
+
+        ]
+
+    }
+
+];
